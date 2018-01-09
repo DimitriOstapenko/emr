6b5ddcd865a5449235c33a1d8aada13902b90fc7
dosta <dosta@me.com>
2018-01-08 21:44:51 -0500

    find box; datepicker; find patients controller/views changes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,12 +1,26 @@
 class Patient < ApplicationRecord
-	attr_accessor :full_name
+	attr_accessor :full_name, :age, :sex_str
 	validates :lname, presence: true, length: { maximum: 50 }
 	validates :fname, presence: true, length: { maximum: 50 }
-	validates :ohip_num, presence: true, length: { is: 10 }, numericality: { only_integer: true }
+	validates :ohip_num, presence: true, length: { is: 10 },
+	           numericality: { only_integer: true },
+                   uniqueness: { case_sensitive: false }
 	validates :dob, presence: true
+	validates :phone, presence: true, length: { is: 10 },
+	           numericality: { only_integer: true }
 
-def full_name
+  def full_name
     [fname, lname].join(', ')
-end
+  end
+
+  def age
+    now = Date.today
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
+  def sex_str
+      ssex = {1 =>'M', 2=>'F'}
+      ssex[sex]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  post '/patients',  to: 'patients#index'
 
   resources :users
   resources :patients
