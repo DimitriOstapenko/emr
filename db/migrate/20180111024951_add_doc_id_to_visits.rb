c3e5c27b38b982eefd64f8f93a2c0924a5ed538d
dosta <dosta@me.com>
2018-01-16 11:48:09 -0500

    doc_id fkey removed from visits

--- db/migrate/20180111024951_add_doc_id_to_visits.rb
+++ db/migrate/20180111024951_add_doc_id_to_visits.rb
@@ -1,6 +1,6 @@
 class AddDocIdToVisits < ActiveRecord::Migration[5.1]
   def change
     add_column :visits, :doc_id, :integer
-    add_foreign_key :visits, :doctors
+#    add_foreign_key :visits, :doctors
   end
 end
