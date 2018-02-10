e7a8b5def28cf770f16cfa3a5d7a03ec2b805b25
Dmitri Ostapenko <Dosta@me.com>
2018-02-10 12:25:26 -0500

    set true for active

--- db/seeds/08_disable_not_used_proc_codes.rb
+++ db/seeds/08_disable_not_used_proc_codes.rb
@@ -9,11 +9,12 @@ Procedure.all.each do |p|
 
   if Billing.find_by(proc_code: p.code)
     puts "found: #{p.code} - kept"
+    p.active = 1 
   else
     puts "not found in billing: #{p.code} - disabled" 
     p.active = 0 
-    p.save
   end
+  p.save
 
 end
 
