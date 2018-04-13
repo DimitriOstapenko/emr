6620eb3b7c4525159fd79eb5f78d1e2b68d36d6b
Dmitri Ostapenko <Dosta@me.com>
2018-04-13 09:27:14 -0400

    diagnoses - back to nnn.nnn

--- db/seeds/02_diagnoses.rb
+++ db/seeds/02_diagnoses.rb
@@ -6,18 +6,19 @@ require_relative '../../config/environment'
 require 'date'
 require 'csv'
 
-puts "About to seed diagnoses table. Validity checks for all but :code should be off"
+puts "About to seed diagnoses table. Validity checks for all but :code should be off."
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'diagfile_data.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
 Diagnosis.destroy_all 
 
 csv.each do |row|
-  diag_code = row['diag_code'][0..2]
+#  diag_code = row['diag_code'][0..2]
+  diag_code = row['diag_code']
   diag = Diagnosis.new   code:  diag_code,
 		   	 descr: row['diag_desc'],
 		         prob_type: row['prob_type']
-  if diag.save		         
+  if diag.save(validate: false)
 	  puts "#{diag.id} #{diag.code} saved"
   else
 	  puts 'Problem code: ', diag.inspect
