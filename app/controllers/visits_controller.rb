1a68537db772e4189df38e732781420acf89b043
dosta <dosta@me.com>
2018-06-16 22:57:16 -0400

    visit save/update redirect - changed back to patient; Notes panel; billing shows previously unbilled by def; datepicker arrows added

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -43,7 +43,8 @@ class VisitsController < ApplicationController
       @patient.last_visit_date = @visit.created_at	    
       @patient.save
       flash[:success] = "Visit saved"
-      redirect_to edit_patient_visit_path(@patient,@visit)
+      redirect_to @patient
+#      redirect_to edit_patient_visit_path(@patient,@visit)
     else
       render 'new'
     end
@@ -111,7 +112,8 @@ class VisitsController < ApplicationController
       set_visit_fees( @visit )
       @visit.save
       flash[:success] = "Visit updated"
-      redirect_back(fallback_location: @patient)
+ #     redirect_back(fallback_location: @patient)
+      redirect_to @patient
     else
       render 'edit'
     end
