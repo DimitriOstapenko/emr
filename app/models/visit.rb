b7622e5cda4b34c042ba91df7a92f12b252e699d
dosta <dosta@me.com>
2018-07-02 08:31:37 -0400

    2 bugs fixed

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -148,7 +148,7 @@ class Visit < ApplicationRecord
 
 # Short version of diagnostic code
   def diag_scode
-	  diag_code[0,3].rjust(3,'0') rescue ''
+	  diag_code[0,3].rjust(3,'0') if diag_code
   end
 
 # Return diagnosis description
