1473ac4587717c135b4b88f9ad2a15144e968140
Dimitri Ostapenko <dosta@me.com>
2018-07-11 12:17:28 -0400

    reconciliation script

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,3 +1,28 @@
 class Claim < ApplicationRecord
 	has_many :services, dependent: :destroy, inverse_of: :claim
+
+# Total claim amount submitted (integer)
+ def amt_subm
+     services.sum(:amt_subm)
+ end 
+
+# Total claim amount billed (integer) 
+ def amt_paid
+     services.sum(:amt_paid)
+ end
+ 
+# Floating point, real total claim amount submitted
+ def damt_subm
+     amt_subm.to_f/100 rescue 0.0
+ end 
+
+# Floating point, real total claim amount paid
+ def damt_paid 
+     amt_paid.to_f/100 rescue 0.0
+ end 
+
+# Visit date, from the first service 
+ def date
+     services.first.svc_date rescue nil
+ end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,15 @@
 class Service < ApplicationRecord
 	belongs_to :claim, inverse_of: :services
+
+
+# Floating point, real amount submitted
+def damt_subm
+    (amt_subm/100).to_f rescue 0.0 
+end
+
+# Floating point, real amount paid
+def damt_paid
+    (amt_paid/100).to_f rescue 0.0 
+end
+
 end
