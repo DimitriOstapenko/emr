c8cb7ce1718d114b91822e66d727995b01c7e236
dosta <dosta@me.com>
2018-08-05 22:30:09 -0400

    tweaks to reports; visit form, daysheet and billing

--- app/controllers/daysheet_controller.rb
+++ app/controllers/daysheet_controller.rb
@@ -6,52 +6,43 @@ class DaysheetController < ApplicationController
 #        before_action :admin_user, only: :destroy
 	
   def index
-      @date = Date.parse(params[:date]) rescue Date.today
+      @date = Date.parse(params[:date]) rescue nil
       store_location
+      if @date.present?
+        @daysheet = Visit.where("date(entry_ts) = ?", @date) 
+      else 
+        @daysheet = Visit.where(status: [ARRIVED,ASSESSED,READY,ERROR] )
+	@date = Date.today
+      end
       
       @docs_visits = Visit.where("date(entry_ts) = ?",@date).group('doc_id').reorder('').size
       @docs = Doctor.find(@docs_visits.keys) rescue []
+	 
       if params[:doc_id]
-         @daysheet = Visit.where("date(entry_ts) = ?", @date).where(doc_id: params[:doc_id])
+         @daysheet = @daysheet.where(doc_id: params[:doc_id])
          d = Doctor.find(params[:doc_id])
-         doctor = "Dr. #{d.lname}" if !d.nil?
+         doctor = "Dr. #{d.lname}" if d.present?
+         flashmsg = "Daysheet for #{doctor} : #{@daysheet.count} visits, "
       else
-	 @daysheet = Visit.where("date(entry_ts) = ?", @date).where(status: (ARRIVED..PAID))
-	 @previously_unbilled = Visit.where("entry_ts <? AND status=?", Date.today, ARRIVED)
+         flashmsg = "#{@daysheet.count}  #{'visit'.pluralize(@daysheet.count)},"
       end
 
-      @insured_svcs = @cash_svcs = @ifh_svcs = 0
+      @insured_svcs = @cash_svcs = @ifh_svcs = @total_cash = 0
       @daysheet.map{|v| @insured_svcs += v.total_insured_services}
       @daysheet.map{|v| @cash_svcs += v.total_cash_services}
+      @daysheet.map{|v| @total_cash += v.total_cash}
       @daysheet.map{|v| @ifh_svcs += v.total_ifh_services}
       if @daysheet.any?
 	 @daysheet = @daysheet.reorder(sort_column + ' ' + sort_direction).paginate(page: params[:page])
-	 flash.now[:info] = "Daysheet for #{@date} : #{@daysheet.count} visits. Insured services: #{@insured_svcs}; Cash services: #{@cash_svcs}; IFH services: #{@ifh_svcs}"
-         render 'index'
-      else
-	 if @previously_unbilled.present? && @date == Date.today
-	    flash.now[:info] = "No visits were found for today, but there are #{@previously_unbilled.count} previously unbilled visits:"
-	    @daysheet = @previously_unbilled.reorder(sort_column + ' ' + sort_direction).paginate(page: params[:page])
-	    render 'index'
-	 else  
+	 flash.now[:info] = "#{flashmsg} #{@insured_svcs} insured #{'service'.pluralize(@insured_svcs)}; #{@cash_svcs} cash #{'service'.pluralize(@cash_svcs)} (#{sprintf('$%.2f',@total_cash)}); #{@ifh_svcs} IFH #{'service'.pluralize(@ifh_svcs)}"
+
+#         render 'index'
+      else  
 	    flash.now[:info] = 'No visits were found for date ' + @date.inspect 
 	    render  inline: '', layout: true
-	 end
       end
+      
   end
-
-#  def set_doctor 
-#      doc_id = params[:doc_id]
-#      id = params[:id]
-#      if doc_id
-#	 set_doc_session( doc_id )
-#	 doc = Doctor.find( doc_id ) || Doctor.new()
-#	 flash[:info] = "Current Doctor set to Dr. #{doc.lname}"
-#	 redirect_back_or( daysheet_url )
-#      else
-#	 render '_set_doctor'
-#      end
-#  end
 
   def sort_column
           Visit.column_names.include?(params[:sort]) ? params[:sort] : "entry_ts"
