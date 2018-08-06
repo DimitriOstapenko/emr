6c444684370c095061756b174c9b056e4e5e9c0d
dosta <dosta@me.com>
2018-08-05 22:32:27 -0400

    date>=

--- app/controllers/paystubs_controller.rb
+++ app/controllers/paystubs_controller.rb
@@ -12,7 +12,7 @@ class PaystubsController < ApplicationController
     (year,month) = Paystub.order(:year, :month).limit(1).pluck('year,month').first rescue nil
     paystub_date = Date.new(year,month) rescue  Date.today - 1.month
     paystub_year_month = paystub_date.strftime('%Y%m').to_i rescue 0
-    @can_generate_new_paystubs = @latest_pay_date.strftime('%Y%m').to_i > paystub_year_month
+    @can_generate_new_paystubs = @latest_pay_date.strftime('%Y%m').to_i >= paystub_year_month
     suff = @can_generate_new_paystubs ? 'Can generate new paystubs': 'Cannot generate new paystubs - already up to date'
 
     flash.now[:info] = "Latest processed RA file: #{@latest_ra_file}; Payment issued #{@latest_pay_date} " + suff
