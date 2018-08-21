ba4727c1924f43374007e64209932659c3ff4413
Dimitri Ostapenko <dosta@me.com>
2018-08-21 10:31:16 -0400

    Passenger fix

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -8,9 +8,13 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog
-  else 
+  else
     storage :file
   end
+   
+  # Choose what kind of storage to use for this uploader:
+  # storage :fog   # ( done in initializer )
+  # storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,15 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region:                ENV['S3_REGION'],
+      aws_access_key_id:     'AKIAJ7HZWQRXMH77F4TA',  #ENV['S3_KEY'],
+      aws_secret_access_key: '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', #ENV['S3_SECRET'],
+      region:                'us-east-1' #ENV['S3_REGION'],
     }
-    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  =  'scwalkin' #ENV['S3_BUCKET']
     config.fog_public     = false
     config.fog_use_ssl_for_aws = true 
   else
-    config.storage :file
+    config.storage = :file
     config.asset_host = 'http://localhost:3000'
   end
 end
