00ad55f07cabcf65c57a577fc637ed51144e8bed
dosta <dosta@me.com>
2018-08-23 18:45:54 -0400

    redirect to patient from new visit; procedure validation tuning

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -3,8 +3,9 @@ class Procedure < ApplicationRecord
         
 	attr_accessor :full_name, :age, :cardstr #, :prov_fee_real, :spec_fee_real, :ana_fee_real, :non_ana_fee_real
 
+	VALID_PCODE_REGEX = /\A[a-z]\d{3}[a-z]\z/i
 	validates :code, presence: true, length: { maximum: 10 }, uniqueness: { case_sensitive: false }
-#        validates :qcode, length: { maximum: 50 }, allow_blank: true
+	validates :code, format: { with: VALID_PCODE_REGEX, message: "for HCP procedure can only be in format <letter><3_digit_number><letter>" }, if: Proc.new { |a| (a.ptype == 1)}
 	validates :ptype, presence: true, length: { maximum: 5 }
 	validates :descr, length: { maximum: 100 }, presence: true
 	validates :cost, presence: true, numericality: true #{greater_than_or_equal_to: 0} 
@@ -14,17 +15,11 @@ class Procedure < ApplicationRecord
 	validates :diag_req, inclusion: { in: [true, false] }, allow_blank: true
 	validates :ref_req, inclusion: { in: [true, false] }, allow_blank: true
 	validates :percent, numericality: true, allow_blank: true 
-#	validates :eff_date, allow_nil: true
-#	validates :term_date, allow_nil: true
 
 	before_validation { code.upcase! rescue nil }
 	before_validation { code.strip! rescue nil }
 	
 	before_save { self.prov_fee = (cost*100).to_i  }
-#	before_save { self.ass_fee = (ass_fee*100).to_i  }
-#	before_save { self.spec_fee = (spec_fee*100).to_i  }
-#	before_save { self.ana_fee = (ana_fee*100).to_i  }
-#	before_save { self.non_ana_fee = (non_ana_fee*100).to_i  }
 	
 def prov_fee_real
   sprintf '%.2f', prov_fee/100.0
