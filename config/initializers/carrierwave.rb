54f14640d469e534a2dbfa69f90f1bcd3f031d10
Dimitri Ostapenko <dosta@me.com>
2018-09-12 08:34:52 -0400

    removed globals, S3 credentials from carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,15 @@
+require_relative 'globals'
+
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'AKIAJ7HZWQRXMH77F4TA',  #ENV['S3_KEY'],
-      aws_secret_access_key: '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', #ENV['S3_SECRET'],
-      region:                'us-east-1' #ENV['S3_REGION'],
+      aws_access_key_id:      S3_KEY_ID, 
+      aws_secret_access_key:  S3_KEY, 
+      region:                 S3_REGION 
     }
-    config.fog_directory  =  'scwalkin' #ENV['S3_BUCKET']
+    config.fog_directory  =  S3_BUCKET
     config.fog_public     = false
     config.fog_use_ssl_for_aws = true 
   else
