4ecc12b09cafe3c7f01bd45a834834649e74e34d
Dimitri Ostapenko <dosta@me.com>
2018-09-19 13:52:17 +0200

    EDT claim generation plugged into billings controller; EDT files index page created

--- lib/my.rb
+++ lib/my.rb
@@ -1,9 +1,79 @@
+# 2 Modules under My:
 #
-# Controller pdf forms functionality factored out
-#
+# 	- EDT: Functionality related to EDT claims submission
+# 	- Forms: All pdf forms and docs
+# 
 
 module My
 
+#--------------------------------
+# EDT files related functionality
+#--------------------------------
+module EDT
+
+# This is first record in every batch	  
+# edt_id: id of new empty edt record; v: first visit in a batch
+def heb_record( edt_id, v )
+  cre_date = Date.today.strftime("%Y%m%d")
+  batch_id = edt_id.to_s.rjust(4,'0')
+  "HEBV03 #{cre_date}#{batch_id}#{' '*6}#{GROUP_NO}#{v.doctor.provider_no}00".ljust(79,' ') + "\r\n"
+end
+
+# Claim header record. One per claim
+def heh_record(v, pat)
+  if pat.pat_type == RMB_PATIENT
+    "HEH#{' '*12}#{pat.dob.strftime("%Y%m%d")}#{v.id.to_s.rjust(8,'0')}RMBP".ljust(79,' ') +"\r\n"
+  else
+    "HEH#{pat.ohip_num}#{pat.ohip_ver}#{pat.dob.strftime("%Y%m%d")}#{v.id.to_s.rjust(8,'0')}HCPP".ljust(79,' ') +"\r\n"
+  end
+end
+
+# HCP claim record
+def het_record(v, s)
+  "HET#{s[:pcode]}  #{(s[:fee]*s[:units]*100).to_i.to_s.rjust(6,'0')}#{s[:units].to_s.rjust(2,'0')}#{v.date_str}#{v.diag_code.to_i.to_s.rjust(3,'0')}".ljust(79,' ') + "\r\n"
+end
+
+# RMB claim record
+def her_record(pat)
+  "HER#{pat.ohip_num.ljust(12,' ')}#{pat.lname[0,9].ljust(9,' ')}#{pat.fname[0,5].ljust(5,' ')}#{DIGSEXES[pat.sex]}#{pat.hin_prov}".ljust(79,' ') + "\r\n"
+end
+
+# Totals. Final record in a batch
+def hee_record( heh_count, her_count, het_count )
+  "HEE#{heh_count.to_s.rjust(4,'0')}#{her_count.to_s.rjust(4,'0')}#{het_count.to_s.rjust(5,'0')}".ljust(79,' ') + "\r\n"
+end
+
+# Generate claim file content
+  def generate_claim_for_doc( edt_id, filename, visits )
+      claims_count = rmb_count = hcp_count = ttl_fee = 0; body = ''
+      body << heb_record(edt_id, visits.first) 
+      visits.all.each do |v| 
+        pat = Patient.find(v.patient_id)
+	ttl_fee += v.total_insured_fees
+	if v.hcp_services? 
+          body << heh_record(v,pat) 
+	  claims_count += 1
+#!!	  v.update_attribute(:status, BILLED) 
+	  v.update_attribute(:export_file, filename) 
+	  if v.bil_type == RMB_BILLING  		# only 1 RMB claim supported per visit right now	
+	    body << her_record(pat)
+	    rmb_count += 1
+	  end
+	end
+        v.services.each do |svc|
+          next unless hcp_procedure?(svc[:pcode])
+          body << het_record(v, svc)
+          hcp_count += 1
+        end
+      end
+      body << hee_record(claims_count, rmb_count, hcp_count)
+      return [body, claims_count, ttl_fee]
+  end
+end # EDT module
+
+#-------------------------------------------      
+# All Pdf forms/documents are generated here  
+#-------------------------------------------
   module Forms
 
     require 'prawn'
@@ -784,7 +854,7 @@ private
     end
   end
 
-  end # Forms
+  end # Forms module
 
 end # My
 
