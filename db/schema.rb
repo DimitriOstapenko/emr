94d199f149b603d77fa7badcfe7850303f4213ff
dosta <dosta@me.com>
2018-11-06 20:36:15 -0500

    bundle update; AWS off
    AWS disabled for now;

--- db/schema.rb
+++ db/schema.rb
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(version: 2018_10_14_081619) do
     t.string "moh_group_id"
     t.integer "visit_id"
     t.string "ra_file"
-    t.date "date_paid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date_paid"
     t.index ["accounting_no"], name: "index_claims_on_accounting_no"
     t.index ["claim_no"], name: "index_claims_on_claim_no"
   end
