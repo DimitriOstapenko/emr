953dbcb4143d38099be60e9e12ac9e04f5c46cf5
Dimitri Ostapenko <dosta@me.com>
2018-11-12 14:13:41 -0500

    migration modified for postgres

--- db/migrate/20181111210907_add_columns_to_referrals.rb
+++ db/migrate/20181111210907_add_columns_to_referrals.rb
@@ -3,7 +3,7 @@ class AddColumnsToReferrals < ActiveRecord::Migration[5.2]
     add_column :referrals, :to_phone, :string
     add_column :referrals, :to_fax, :string
     add_column :referrals, :to_email, :string
-    change_column :referrals, :to, :integer
-    rename_column :referrals, :to, :to_doctor_id
+    remove_column :referrals, :to, :string
+    add_column :referrals, :to_doctor_id, :integer
   end
 end
