531e107110652b23fb1f449537aa8cc15acf8166
Dimitri Ostapenko <dosta@me.com>
2020-04-06 13:51:52 +0200

    meta tags added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,7 +30,7 @@ end
 
 # Returns the full title on a per-page basis.
     def full_title(page_title = '')
-      base_title = "Walk-In EMR"
+      base_title = "OHIP Covered Telemedicine Visits"
       if page_title.empty?
         base_title
       else
