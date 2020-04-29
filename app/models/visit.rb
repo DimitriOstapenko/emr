52bd62b13952b97ae8299b42b19b5be122f755fd
Dimitri Ostapenko <dosta@me.com>
2020-04-29 12:59:48 +0200

    few typos; visit note default not set for patient; patient form simplified

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -335,7 +335,7 @@ private
           self.bil_type ||= HCP_BILLING
 	  self.duration ||= 10
           self.proc_code ||= 'K081A' # televisit!!
-          self.notes ||= 'Verbal consent was obtained. '
+          self.notes ||= 'Verbal consent was obtained. ' unless pat.user && pat.user.patient?
   end
 
 end
