e86c2f569b4637d17883b20b9f0dfcb24eff954f
Dimitri Ostapenko <dosta@me.com>
2020-05-30 20:58:04 +0200

    login message after confirmation changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ApplicationMailer
 # Send new registration notification to staff members
    def new_registration(user)
      @user = user
-     emails = User.where('role=?', STAFF_ROLE).pluck(:email) rescue REPLY_TO
+     emails = User.where('role=?', ADMIN_ROLE).pluck(:email) rescue REPLY_TO
      mail to: emails, subject: "User Account Created", from: 'admin@drlena.com'
    end
 
