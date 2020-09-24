061253ae878897d4e8d8dea2651d5c932e298cf9
Dimitri Ostapenko <dosta@me.com>
2020-09-24 11:10:16 +0200

    signin after password reset

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -3,8 +3,11 @@ class MyRegistrationsController < Devise::RegistrationsController
 # Log in user after registration
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource)
+    redirect_to patient_path(current_user.patient)
+  end
 
-#    redirect_to visits_path
+  def after_update_path_for(resource)
+    sign_in(resource)
     redirect_to patient_path(current_user.patient)
   end
 
