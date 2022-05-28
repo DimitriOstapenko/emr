65c2a282697adc40ad0cd2e49f6dbd2c32dd36f8
rails <dosta@me.com>
2022-05-28 10:01:50 -0400

    update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,11 @@ class StaticPagesController < ApplicationController
   def home
 #    clear_pat_session
     
-    redirect_to current_user.patient if current_user && current_user.patient
-    redirect_to new_user_session_path if current_patient && current_patient.user && current_patient.user.confirmed_at
+    if current_user && current_user.patient
+      redirect_to current_user.patient 
+    elsif current_patient && current_patient.user && current_patient.user.confirmed_at
+      redirect_to new_user_session_url
+    end
 
 #    flash[:info] = "pat #{current_patient.inspect}  user : #{current_user.inspect}"
   end
