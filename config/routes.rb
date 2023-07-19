bcaddd1c2b1b4c361ca1797244fc5f558d39f250
Dimitri Ostapenko <dosta@me.com>
2023-07-19 11:31:09 +0200

    Revert "other clinics link"
    This reverts commit c6f0f6b079a949b59a4a153a75f81ec1f3d00772.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  get '/other_clinics', to: 'static_pages#other_clinics'
 #   root 'daysheet#index'
   
   devise_for :users, controllers: { registrations: "my_registrations", sessions: 'users/sessions' }
--- lib/tasks/import.rake
+++
@@ -1,6 +0,0 @@
-namespace :import do
-  desc "TODO"
-  task data: :environment do
-  end
-
-end
