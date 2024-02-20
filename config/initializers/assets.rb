038d1b295e96fcc96f0ebaff4cf3d6360a5eb0cf
Dimitri Ostapenko <dosta@me.com>
2024-02-20 16:04:58 +0100

    form format

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,5 +4,5 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( application-mailer.scss )
+#Rails.application.config.assets.precompile += %w( application-mailer.scss )
 
