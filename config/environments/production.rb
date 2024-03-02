f8d32dc24da1326cb8a3cfa4ff77299dc875e90b
Dmitri Ostapenko <dosta@me.com>
2024-03-02 20:42:13 +0100

    devise form; mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.default_url_options = { :host => 'drlena.com' }
+   config.action_mailer.default_url_options = { :host => 'backup.drlena.com' }
    config.action_mailer.delivery_method = :sendmail
    config.action_mailer.smtp_settings = {
          address:              'localhost',
